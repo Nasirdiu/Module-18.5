ac1a38bd63547816445489ddba7dc16dd3950851
batch-05 <nasir.joypurhat@gmail.com>
2022-01-27 23:43:28 +0600

    forloop

--- for-loop.js
+++ for-loop.js
@@ -11,6 +11,13 @@
 
 // even num 
 
-for(var num=0; num <= 20; num +=2){
-    console.log(num);
+// for(var num=0; num <= 20; num +=2){
+//     console.log(num);
+// }
+
+var num =[45,50,55,60,65,70,100];
+
+for(var i=0; i<7; i++){
+    var element =num[i];
+    console.log(element);
 }
--- while-loop.js
+++ while-loop.js
@@ -1,9 +1,13 @@
 var rostGiven=0;
 
-while(rostGiven < 7 ){
-console.log('please give me');
-rostGiven++;
-console.log(rostGiven);
+// while(rostGiven < 7 ){
+// console.log('please give me');
+// rostGiven++;
+// console.log(rostGiven);
+// }
+
+
+while(rostGiven<7){
+    rostGiven++;
+    console.log(rostGiven);
 }
-
-
